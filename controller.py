d3a50ac33a1dbeeb09e26a8961eaed4cbe52f341
Median72 <medvivan11@gmail.com>
2025-05-24 18:11:15 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,1 +1,39 @@
+from view import *
+from model import *
+class Controller:
+    def __init__(self, root):
+        self.model = User()
+        self.view = View(root)
+        self._setup_handlers()
 
+    def _setup_handlers(self):
+        self.view.set_handlers(
+            self.handle_register,
+            self.handle_login,
+            self.handle_logout
+        )
+
+    def handle_register(self):
+        username, password = self.view.get_credentials()
+        if not username or not password:
+            self.view.show_message("Все поля должны быть заполнены!", "red")
+            return
+
+        if self.model.add_user(username, password):
+            self.view.show_success_page(f"Добро пожаловать, {username}!")
+        else:
+            self.view.show_message("Пользователь уже существует!", "red")
+
+    def handle_login(self):
+        username, password = self.view.get_credentials()
+        if not username or not password:
+            self.view.show_message("Все поля должны быть заполнены!", "red")
+            return
+
+        if self.model.check_user(username, password):
+            self.view.show_success_page(f"Рады снова видеть, {username}!")
+        else:
+            self.view.show_message("Неверные учетные данные!", "red")
+
+    def handle_logout(self):
+        self.view.show_login_page()
